17ba42e7fd490c8f6f1dfe1ca7c494e6a3c1c608
ayenyeinsan <aye2904@gmail.com>
2024-06-27 22:45:53 +0700

    9 to 9.6

---
+++ components/ProductDisplay.js
@@ -0,0 +1,124 @@
+const { ref, computed } = Vue;
+const productDisplay = {
+  template:
+    /*html*/
+    `
+    <div class="product-display">
+        <div class="product-container">
+          <div class="product-image" >
+            <!-- 7.6 -->
+            <img :src="image" :class="{'out-of-stock-img': !inStock} " />
+          </div>
+        </div>
+      </div>
+      <div class="product-info">
+        <h1>{{ title}}</h1>
+        <!-- 3.6  -->
+        <a :href="productLink">product Link</a>
+
+        <p v-if="inventory > 10">In stock</p>
+
+        <p v-else-if="inventory <=10 && inventory > 0">Almost out of Stock</p>
+        <p v-else>Out of Stock</p>
+        <!-- 4.9  + 8.5 -->
+        <p v-if="onSale">{{salepost}}</p>
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <!-- 8.4 -->
+        <div
+          v-for="(variant,index) in variants"
+          :key="variant.id"
+          @mouseover="updateVariant(index)"
+          class="color-circle"
+          :style="{backgroundColor:variant.color}"
+        >
+          {{variant.color}}
+        </div>
+           <!-- 5.5 -->
+        <ul style="display: flex">
+          <span>Sizes: </span>
+          <li v-for="size in  sizes">{{ size }},</li>
+        </ul>
+        <button class="button" :disabled="!inStock" :class="{disabledButton: !inStock}" @click="addToCart">Add To Cart</button>
+        
+      </div>
+       <!-- 6.7 -->
+      <button class="button" @click="updateStock">Buy</button>
+   
+    `,
+  setup() {
+    const product = ref("Boots");
+    const brand = ref("SE 331");
+    const image = computed(() => {
+      return variants.value[selectedVariant.value].image;
+    });
+    const productLink = ref("https://www.camt.cmu.ac.th/index.php/th/");
+    const inStock = computed(() => {
+      return variants.value[selectedVariant.value].quantity;
+    });
+    const inventory = ref(3);
+    const onSale = ref(true);
+
+    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+    const variants = ref([
+      {
+        id: 2234,
+        color: "green",
+        image: "./assets/images/socks_green.jpg",
+        quantity: 50,
+      },
+      {
+        id: 2235,
+        color: "blue",
+        image: "./assets/images/socks_blue.jpg",
+        quantity: 0,
+      },
+    ]);
+    const selectedVariant = ref(0);
+    const sizes = ref(["S", "M", "L"]);
+    const title = computed(() => {
+      return brand.value + " " + product.value;
+    });
+
+    const salepost = computed(() => {
+      return brand.value + " " + product.value + " " + "is on sale";
+    });
+    const cart = ref(0);
+    const addToCart = () => {
+      cart.value += 1;
+    };
+    const updateImage = (variantImage) => {
+      image.value = variantImage;
+    };
+    const updateStock = () => {
+      inventory.value -= 1;
+      if (inventory.value == 0) {
+        inStock.value = false;
+      }
+    };
+    const updateVariant = (index) => {
+      selectedVariant.value = index; //0
+    };
+    return {
+      title,
+      salepost,
+      brand,
+      product,
+      image,
+      productLink,
+      inStock,
+      inventory,
+      onSale,
+      details,
+      variants,
+      sizes,
+      cart,
+      addToCart,
+      updateImage,
+      updateStock,
+      updateVariant,
+    };
+  },
+};
+export default productDisplay;
--- main.js
+++ main.js
@@ -1,68 +1,14 @@
-const { createApp, ref,computed } = Vue;
+const { createApp, ref , computed } = Vue;
+import productDisplay from './components/ProductDisplay.js';
 
-createApp({
+const app = createApp({
   setup() {
-    const product = ref("Boots");
-    const brand = ref ('SE 331');
-    const image = computed(()=>{
-        return variants.value[selectedVariant.value].image
-    })
-    const productLink = ref("https://www.camt.cmu.ac.th/index.php/th/");
-    const inStock = computed(()=>{
-        return variants.value[selectedVariant.value].quantity
-    })
-    const inventory = ref(3);
-    const onSale =ref(true);
-    
-    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
-    const variants = ref([
-      { id: 2234, color: "green", image:'./assets/images/socks_green.jpg' ,quantity:50},
-      { id: 2235, color: "blue",image :'./assets/images/socks_blue.jpg' ,quantity :0},
-    ]);
-    const selectedVariant = ref(0);
-    const sizes = ref(["S","M","L"])
-    const title=computed(()=>{
-        return brand.value+' '+ product.value
-    })
-   
-    const salepost=computed(()=>{
-        return brand.value+' '+ product.value +' '+ "is on sale"
-    })
     const cart = ref(0)
-    const addToCart=()=>{
-        cart.value+=1
+    return{
+        cart
     }
-     const updateImage =(variantImage)=>{
-        image.value = variantImage
-     }
-     const updateStock=()=>{
-        inventory.value -=1
-        if(inventory.value == 0){
-            inStock.value = false;
-        }
-     }
-     const updateVariant=(index)=>{
-        selectedVariant.value = index //0
-     } 
-    return {
-      title,
-      salepost,
-      brand,
-      product,
-      image,
-      productLink,
-      inStock,
-      inventory,
-      onSale,
-      details,
-      variants,
-      sizes,
-      cart,
-      addToCart,
-      updateImage,
-      updateStock,
-      updateVariant,
-
-    };
   },
-}).mount("#app");
+})
+
+app.component('product-display',productDisplay)
+app.mount('#app')
