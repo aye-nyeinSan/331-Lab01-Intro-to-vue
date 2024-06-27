fda6af174b70e21c792cc789b418ac49ae4541fa
ayenyeinsan <aye2904@gmail.com>
2024-06-27 18:31:04 +0700

    8.4

--- main.js
+++ main.js
@@ -4,16 +4,21 @@ createApp({
   setup() {
     const product = ref("Boots");
     const brand = ref ('SE 331');
-    const image = ref("./assets/images/socks_green.jpg");
+    const image = computed(()=>{
+        return variants.value[selectedVariant.value].image
+    })
     const productLink = ref("https://www.camt.cmu.ac.th/index.php/th/");
-    const inStock = ref(true);
+    const inStock = computed(()=>{
+        return variants.value[selectedVariant.value].quantity
+    })
     const inventory = ref(3);
     const onSale = ref(true);
     const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     const variants = ref([
-      { id: 2234, color: "green", image:'./assets/images/socks_green.jpg' },
-      { id: 2235, color: "blue",image :'./assets/images/socks_blue.jpg' },
+      { id: 2234, color: "green", image:'./assets/images/socks_green.jpg' ,quantity:50},
+      { id: 2235, color: "blue",image :'./assets/images/socks_blue.jpg' ,quantity :0},
     ]);
+    const selectedVariant = ref(0);
     const sizes = ref(["S","M","L"])
     const title=computed(()=>{
         return brand.value+' '+ product.value
@@ -31,6 +36,9 @@ createApp({
             inStock.value = false;
         }
      }
+     const updateVariant=(index)=>{
+        selectedVariant.value = index
+     }
     return {
       title,
       image,
@@ -44,7 +52,9 @@ createApp({
       cart,
       addToCart,
       updateImage,
-      updateStock
+      updateStock,
+      updateVariant,
+
     };
   },
 }).mount("#app");
