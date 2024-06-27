41fef2d01ec3c6ca0f9f8e27f5d99ab21a6bdae1
ayenyeinsan <aye2904@gmail.com>
2024-06-27 18:04:24 +0700

    6,7

--- main.js
+++ main.js
@@ -6,14 +6,28 @@ createApp({
     const image = ref("./assets/images/socks_green.jpg");
     const productLink = ref("https://www.camt.cmu.ac.th/index.php/th/");
     const inStock = ref(true);
-    const inventory = ref(100);
+    const inventory = ref(3);
     const onSale = ref(true);
     const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     const variants = ref([
-      { id: 2234, color: "green" },
-      { id: 2235, color: "blue" },
+      { id: 2234, color: "green", image:'./assets/images/socks_green.jpg' },
+      { id: 2235, color: "blue",image :'./assets/images/socks_blue.jpg' },
     ]);
     const sizes = ref(["S","M","L"])
+
+    const cart = ref(0)
+    const addToCart=()=>{
+        cart.value+=1
+    }
+     const updateImage =(variantImage)=>{
+        image.value = variantImage
+     }
+     const updateStock=()=>{
+        inventory.value -=1
+        if(inventory.value == 0){
+            inStock.value = false;
+        }
+     }
     return {
       product,
       image,
@@ -23,7 +37,11 @@ createApp({
       onSale,
       details,
       variants,
-      sizes
+      sizes,
+      cart,
+      addToCart,
+      updateImage,
+      updateStock
     };
   },
 }).mount("#app");
